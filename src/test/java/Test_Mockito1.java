1bc75759223ba331c8c8716ac6a385ade8367517
IrekNabiullin <08git08@mail.ru>
2021-03-07 00:22:24 +0300

    Initial commit

---
+++ src/test/java/Test_Mockito1.java
@@ -0,0 +1,8 @@
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class Test_Mockito1
+{
+    @Mock
+    ICalculator mcalc;
+}
---
+++ src/test/java/Test_Mockito2.java
@@ -0,0 +1,6 @@
+import org.mockito.Mock;
+import org.mockito.Mockito;
+public class Test_Mockito2
+{
+    ICalculator mcalc = Mockito.mock(ICalculator.class);
+}
