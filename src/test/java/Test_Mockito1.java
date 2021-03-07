c3ac6edbffa5650f2b989eaf4f0cc990d670b3fb
IrekNabiullin <08git08@mail.ru>
2021-03-07 12:20:05 +0300

    Comments added

--- src/test/java/Test_Mockito1.java
+++ src/test/java/Test_Mockito1.java
@@ -1,3 +1,9 @@
+/*
+Чтобы создать Mockito объект можно использовать либо аннотацию @Mock, либо метод mock.
+В двух разных классах (Test_Mockito1, Test_Mockito2) разными способами создаются объекты mcalc
+для имитации интерфейса калькулятора ICalculator.
+ */
+
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
